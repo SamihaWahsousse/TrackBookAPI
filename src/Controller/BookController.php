38751e6e638b0655548a52e55669be74e45a16e4
Samiha Wahsousse <wahsousse.samiha@gmail.com>
2023-03-29 10:06:46 +0200

    add borrowBook API endpoint in Book Controller

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Book;
 use App\Entity\BorrowBook;
 use App\Repository\BookRepository;
+use App\Repository\BorrowBookRepository;
 use App\Repository\UserRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -51,38 +52,62 @@ class BookController extends AbstractController
     {
         $book   = $bookRepository->find($id_book);
         if (!$book) {
-            return $this->json(["Message" => "Book doesn't exist"]);
+            return $this->json(["Message" => "Book doesn't exist"], Response::HTTP_BAD_REQUEST);
         }
 
+        if (!$book->isIsAvailable()) {
+            return $this->json(["Message" => "Book is already borrowed"], Response::HTTP_ACCEPTED);
+        } else {
+
+            $userId = $request->get("id");
+            $user   = $userRepository->find($userId);
+
+
+            $borrow = new BorrowBook();
+            $borrow->setBook($book);
+            $borrow->setUser($user);
+            $borrow->setBorrowDate(new DateTime());
+            $book->setIsAvailable(false);
+
+            $em->persist($borrow);
+            $em->flush();
+            return $this->json(["Message" => "Book borrowed!"]);
+        }
+    }
+
+    #[Route('/api/v1/books/{id_book}/return', name: 'post_books_return', methods: ["POST"])]
+    public function returnBook($id_book, SerializerInterface $serializer, Request $request, BookRepository $bookRepository, UserRepository $userRepository, EntityManagerInterface $em, BorrowBookRepository $borrowBookRepository): Response
+    {
+        $book   = $bookRepository->find($id_book);
         $userId = $request->get("id");
         $user   = $userRepository->find($userId);
 
 
-        $borrow = new BorrowBook();
-        $borrow->setBook($book);
-        $borrow->setUser($user);
-        $borrow->setBorrowDate(new DateTime());
-        $book->setIsAvailable(false);
+        if (!$book->isIsAvailable()) {
+            $borrow = $borrowBookRepository->findOneBy(["id" => $book, "id" => $user]);
+            $timeBorrow = date("Y-m-d H:i:s");
+            $testTime = $borrow->setReturnDate(new DateTime($timeBorrow));
+            // dd($testTime);
+            $book->setIsAvailable(true);
+            $em->persist($book);
+            $em->flush();
 
-        $em->persist($borrow);
-        $em->flush();
-        return $this->json(["Message" => "Book borrowed!"]);
+            return $this->json(["message" => "Book returned"]);
+        } else {
+            return $this->json(["Message" => "Book is available", Response::HTTP_BAD_REQUEST]);
+        }
     }
 }
+/*le premier passage (emprunter le livre)
+-> emprunter un book(action à créer dans la table borrowBook)
+*identifier l'utilisateur qui souhaite faire l'action emprunter
+*identifier le book via son id sur lequel l'utilusateur souhaite faire l'action emprunter
+*on crée un nouveau Objet BorrowBook avec tous ses attributs setUser,setBook,setisAvailable (pour le book)
 
-
-// #[Route('/api/v1/books/{id_book}/returnBook', name: '')]
-
-    // #[Route('/api/v1/books/{id_book}/return', name: 'post_books_unborrow', methods: ["POST"])]
-    //     public function returnBook($id, SerializerInterface $serializer, Request $request, BookRepository $bookRepository, UserRepository $userRepository, EntityManagerInterface $em): Response
-    //     {
-    //         $books = $this->getRepository($id_book);
-    //         $userId= $request->get("id");
-
-    //         if($book->isIsAvailable()){
-    //             return $this->json(["Message"=>""]);
-    //         }
-    // //
-
-    //         return
-    //     }
+*le deuxième passage (retourner le livre)
+-> je vérifie l'id de l'utilisateur 
+-> je recherche l'id du livre objet de l'action retourner,je récupère l'id du spotBooks (àtravers le scan de son qrcode),
+je vérifie son état si isAvailable == false
+alors changer son etat setIsAvailable==true sinon laisser son état comme il est et afficher erreur 
+à l'utilisateur "chemin inexistant"
+*/
